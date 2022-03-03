16ec6ff22478b5367b67cc1256ad84953f2dcbfa
runnerFeng <631819061@qq.com>
2022-03-03 23:08:07 +0800

    update

--- src/main/java/com/feng/chapter1/package-info.java
+++ src/main/java/com/feng/chapter1/package-info.java
@@ -2,7 +2,7 @@
  * @Author jinx
  * @Date 2022/3/3
  * @Desc 1.::方法引用语法引用具名的函数，也可以使用匿名引用（lambda函数）
- *       2.Predicate<T>：使用谓词对boolean的参数行为化
+ *       2.Predicate<T>：使用谓词对boolean的参数做行为参数化
  *       3.串行，并行Stream
  *       4.default:默认方法关键字
  */
